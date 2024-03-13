32dd49602888503680c2d25368ea7c4e1bbe1754
Sarmat <sarmat.karaf.0@gmail.com>
2024-03-13 16:38:59 +0300

    upd

--- 13,03,2024/2485_Find_the_Pivot_Integer.cpp
+++ 13,03,2024/2485_Find_the_Pivot_Integer.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 
+using namespace std;
 
+class solution
+{
+private:
+    int num ;
+public:
+    solution(int n) : num(n) {}
+
+    // int pivotInteger() {
+    //     vector<int> r_sum = {num} ,  l_sum = {0};
+    //     for (int i = 1 , j = num - 1 ; i <= num  ; i++, j--){
+    //         printf("%d -  L : %d  R : %d \n" , i ,l_sum[i-1], r_sum[i-1] );
+    //         l_sum.push_back( l_sum[i-1] + i);
+    //         r_sum.push_back( r_sum[i-1] + j);
+    //     }
+
+    //     for (int i = 0 , j = num  ; i <= num  ; i++, j--){
+    //         if (l_sum[i] == r_sum [j])
+    //             return i+1;
+    //     }
+    //     return -1;
+    // }
+
+    int pivotInteger() {
+        for(int i=1;i<=num;i++){
+             printf("%d -  L : %d  R : %d \n" , i ,((i*(i+1))/2 ), (num*(num+1))/2-( (i*(i+1))/2)+i );
+            if( ((i*(i+1))/2 )== (num*(num+1))/2-( (i*(i+1))/2)+i)
+                return i;
+        }
+        return -1;
+    }
+};
 
 
 
@@ -9,24 +43,9 @@
 
 int main() {
 
-    std::vector<int> arr = {1, 2, 3, 4};
-    int n = arr.size();
-    
+    int n = 8;
+    solution solution(n);
+    cout << solution.pivotInteger();
 
-    std::vector<int> prefixSums(n, 0);
-    
-
-    prefixSums[0] = arr[0]; 
-    for (int i = 1; i < n; ++i) {
-        prefixSums[i] = prefixSums[i - 1] + arr[i];
-    }
-    
-
-    std::cout << "Префиксные суммы: ";
-    for (int i = 0; i < n; ++i) {
-        std::cout << prefixSums[i] << " ";
-    }
-    std::cout << std::endl;
-    
     return 0;
 }
