659c3a155a77d3413db6aad1696c1eb9fd0c7e70
Sarmat <sarmat.karaf.0@gmail.com>
2024-03-14 14:12:21 +0300

    upd

--- 13,03,2024/1679_Max_Number_of_K-Sum_Pairs.cpp
+++ 13,03,2024/1679_Max_Number_of_K-Sum_Pairs.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -14,11 +15,50 @@ public:
     solution(vector<int>& nums , int n) : nums(nums) , k(n) {}
 
     int maxOperations() {
-        for (int i = 0 ; i  < nums.size() ; i++) {
+        // Мое решение Limit time - error
+        // int count = 0;
+        // for (int i = 0 ; i  < nums.size() ; i++) {
+        //     auto it = find(nums.begin(), nums.end(), k-nums[i]);
+        //     if (it != nums.end() && it != nums.begin() + i){
+        //         if(it > nums.begin()+i){
+        //             nums.erase(it);
+        //             nums.erase(nums.begin() + i);
+        //             i--;
+        //         }else {
+        //             nums.erase(nums.begin() + i);
+        //             nums.erase(it);
+        //             i--;
+        //         }
+        //         count++;
+        //     }
+        // }
+        // return count;
+    //_____________________________________________________________
+        // GPT - hash table : memory - error
+        // std::unordered_map<int, int> map;
+        // int count = 0;
+        // for (int num : nums) {
+        //     int complement = k - num;
+        //     if (map[complement] > 0) {
+        //         count++;
+        //         map[complement]--;
+        //     } else {
+        //         map[num]++;
+        //     }
+        // }
+        // return count;
+    //_____________________________________________________________
+        // correct code with two pointers 
+        sort(nums.begin(), nums.end());
+        int l_i = 0 , r_i =nums.size() - 1 , count = 0;
 
-            
+        while (l_i < r_i){
+            int temp = nums[l_i] + nums[r_i];
+            if (temp == k) count++ , l_i++ , r_i--;
+            else if (temp < k) l_i++;
+            else r_i--;
         }
-        
+        return count;
     }
 };
 
@@ -27,8 +67,8 @@ public:
 
 
 int main() {
-    vector<int> nums = {1,2,3,4};
-    int n = 8;
+    vector<int> nums = {3,1,3,4,3};
+    int n = 6;
     solution solution(nums , n);
     cout << solution.maxOperations();
 
