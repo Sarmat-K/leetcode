598a85470d4c5d82c1e100cb05b982813f8d3dba
Sarmat-K <sarmat.karaf.0@gmail.com>
2024-10-25 23:09:54 -0400

    upd

--- 10.25.2024/271_Encode_and_Decode_string.cpp
+++ 10.25.2024/271_Encode_and_Decode_string.cpp
@@ -9,3 +9,55 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+class Code{
+public:
+
+    string encode(vector<string>& strs) {
+        string encode = "";
+        for(const string& word : strs){
+            encode += to_string(word.size()) + '@' + word;
+        }
+        return encode;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decode;
+
+        int i = 0;
+
+        while(i < s.size()){
+
+            int j = i;
+            while(s[j] != '@'){
+                j++;
+            }
+            int lenght = stoi(s.substr(i, j - i)); // забираем длтну слова 
+            i = j + 1; // переход к началу строки 
+            decode.push_back(s.substr(i, lenght));
+            i += lenght;
+
+        }
+        return decode;
+
+    }
+
+};
+
+int main(){
+    Code text;
+    vector<string> vec = {"apple" , "car" , "cat", "sigma"};
+    string enc = text.encode(vec);
+    cout << enc << endl;
+
+    vector<string> dec = text.decode(enc);
+
+    for(const string& word : dec){
+        cout << word << " ";
+    }
+    cout << endl;
+
+    
+
+    return 0;
+}
