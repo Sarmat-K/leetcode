04b383db2562e8d2be5d172a7130f3d73fb2030c
Sarmat-K <sarmat.karaf.0@gmail.com>
2024-10-29 15:39:52 -0400

    upd

--- 10.28.2024/27 _Remove_Element.cpp
+++ 10.28.2024/27 _Remove_Element.cpp
@@ -9,3 +9,15 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) k++;
+            else swap(nums[i], nums[i-k]);
+        }
+        return nums.size()-k;
+    }
+};
