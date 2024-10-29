dfb112922f150d8ee8de8918ebd28587c92ac9bc
Sarmat-K <sarmat.karaf.0@gmail.com>
2024-10-29 18:09:02 -0400

    fix tsk 128

--- 10.26.2024/128_Longest_Consecutive_sequence.cpp
+++ 10.26.2024/128_Longest_Consecutive_sequence.cpp
@@ -6,6 +6,36 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+
+int longestConsecutive(vector<int>& nums) {
+    unordered_set<int> store(nums.begin(), nums.end());
+    int res = 0;
+
+    for(const int& num : nums){
+        int streak = 0;
+        int curr = num;
+
+        while(store.find(curr) != store.end()){
+            streak++;
+            curr++;
+        }
+
+        res = max(res, streak);
+    }
+
+    return res;
+}
+
+int main(){
+    vector<int> nums = {4, 100, 2, 3, 1 ,200};
+
+    int streak = longestConsecutive(nums);
+    cout << streak << endl;
+
+    return 0;
+}
